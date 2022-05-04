8ecbf2f131d545b52fe2d38258ace478fc8d5f88
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-04 15:29:52 +0530

    Added to EmpAttendance

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Employee Wage Computation Program");
+﻿using System;
+namespace EmpWageProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int isPresent = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not Present");
+            }
+        }
+    }
+}
