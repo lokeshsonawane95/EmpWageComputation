5f07e0190aeb36ac8cd991c9230821f136271050
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-04 17:13:06 +0530

    Added to CalcWageForMonth

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -6,26 +6,33 @@ namespace EmpWageProblem
         public const int isFullTime = 1;
         public const int isPartTime = 2;
         public const int empRatePerHour = 20;
+        public const int numOfWorkingDays = 20;
         public static void Main(string[] args)
         {
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int totalEmpWage = 0;
+            for (int day = 0; day < numOfWorkingDays; day++)
             {
-                case isFullTime:
-                    empHrs = 8;
-                    break;
-                case isPartTime:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * empRatePerHour;
+                totalEmpWage += empWage;
+                Console.WriteLine("Employee Wage : " + empWage);
             }
-            empWage = empHrs * empRatePerHour;
-            Console.WriteLine("Employee Wage : " + empWage);
+            Console.WriteLine("Total Employee Wage : " + totalEmpWage);
         }
     }
 }
