d514e3e0c4b7c73010b040870396463798aff5fd
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-04 15:41:57 +0530

    Added to CalcDailyEmpWage

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -6,16 +6,21 @@ namespace EmpWageProblem
         public static void Main(string[] args)
         {
             int isPresent = 1;
+            int empRatePerHour = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == isPresent)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is not Present");
+                empHrs = 0;
             }
+            empWage = empHrs * empRatePerHour;
+            Console.WriteLine("Employee Wage : " + empWage);
         }
     }
 }
