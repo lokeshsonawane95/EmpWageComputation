c5b21e16e135537988c9727d717927b6cf37954f
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-04 15:49:41 +0530

    Added to PartTimeEmpWage

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -5,15 +5,20 @@ namespace EmpWageProblem
     {
         public static void Main(string[] args)
         {
-            int isPresent = 1;
+            int isFullTime = 1;
+            int isPartTime = 2;
             int empRatePerHour = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == isPresent)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == isFullTime)
             {
                 empHrs = 8;
+            }
+            else if (empCheck == isPartTime)
+            {
+                empHrs = 4;
             }
             else
             {
